2b1d5307c1ce095eb16cbb753b0bc8f9d7d8ab65
pedroosorio <pedro.osorio04@inacapmail.cl>
2023-08-22 16:50:26 -0400

    Update project clone netflix with django==4.2.4

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name= 'core'
 
 urlpatterns = [
-    path('', Home.as_view())
+    path('', Home.as_view()),
+    path('profile/', ProfileList.as_view(), name='profile_list'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from  django.utils.decorators  import method_decorator
 
 # In this file, we gonna to create our views, which will be rendered from python file to html, and will be seen by the user
 
 class Home(View):
     def get(self, request, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('core:profile_list')
         return render(request, 'index.html')
+
+@method_decorator(login_required, name='dispatch')
+class ProfileList(View):
+    def get(self, request, **kwargs):
+        profiles =request.user.profile.all()
+        return render(request, 'profileList.html',{
+            'profiles': profiles
+        }
+    )
+
+
+class ProfileCreate(View):
+    pass
